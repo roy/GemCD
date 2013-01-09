d34028fd86d398911582ab3086e974cd7e30b28f
Roy van der Meij <roy@royapps.nl>
2013-01-09 11:21:26 +0100

    quick hack to find gem by partial match

--- lib/rubygems/commands/cd.rb
+++ lib/rubygems/commands/cd.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 class Gem::Commands::CdCommand < Gem::Command
   def initialize
     super 'cd', "CD to the gem directory."
@@ -10,6 +11,9 @@ class Gem::Commands::CdCommand < Gem::Command
   def execute
     gem = options[:args].first
     raise Gem::CommandLineError, 'Usage: gem cd GEM' unless gem
+
+    gem = `gem list | grep #{gem} | awk '{print $1}'`.gsub(/\n/, "")
+    puts "found gem: #{gem}"
     Dir.chdir("#{Gem::Specification.find_by_name(gem).full_gem_path}")
     exec ENV['SHELL']
   end
